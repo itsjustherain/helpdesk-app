b3d569c5b7b5609ba3ef939bba0435794170ec65
eduu <eduivi0078@gmail.com>
2026-06-07 19:01:16 +0200

    Implement AsignacionDAO

---
+++ src/dao/AsignacionDAOImpl.java
@@ -0,0 +1,57 @@
+package dao;
+
+import db.ConexionDB;
+import model.Asignacion;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AsignacionDAOImpl implements AsignacionDAO {
+
+    @Override
+    public void insert(Asignacion asignacion) throws SQLException {
+        String sql = "INSERT INTO asignaciones (incidencia_id, tecnico_id, comentario) VALUES (?, ?, ?)";
+
+        try (Connection conn = ConexionDB.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)) {
+                ps.setInt(1, asignacion.getIncidenciaId());
+                ps.setInt(2, asignacion.getTecnicoId());
+                ps.setString(3, asignacion.getComentario());
+                
+                ps.executeUpdate();
+        }
+    }
+
+    @Override
+    public List<Asignacion> findAll() throws SQLException {
+        String sql = "SELECT id, incidencia_id, tecnico_id, fecha_asignacion, comentario FROM asignaciones";
+        List<Asignacion> asignaciones = new ArrayList<>();
+
+        try (Connection conn = ConexionDB.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    asignaciones.add(new Asignacion(
+                        rs.getInt("id"),
+                        rs.getInt("incidencia_id"),
+                        rs.getInt("tecnico_id"),
+                        rs.getTimestamp("fecha_asignacion").toLocalDateTime(),
+                        rs.getString("comentario")
+                    ));
+                }
+        }
+        return asignaciones;
+    }
+
+    @Override
+    public void delete(int id) throws SQLException {
+        String sql = "DELETE FROM asignaciones WHERE id=?";
+
+        try (Connection conn = ConexionDB.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)) {
+                ps.setInt(1, id);
+                ps.executeUpdate();
+        }
+    }
+}
